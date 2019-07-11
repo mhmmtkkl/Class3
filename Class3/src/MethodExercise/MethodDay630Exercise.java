ce608db42bcfecd03ef0ec4f3f3112f566ced714
mhmmtkkl <makoklu32@gmail.com>
2019-07-10 21:11:52 -0400

    HOMEWORK

--- Class3/src/MethodExercise/MethodDay630Exercise.java
+++ Class3/src/MethodExercise/MethodDay630Exercise.java
@@ -1,14 +1,44 @@
 package MethodExercise;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MethodDay630Exercise {
 
+	
+	public static void main(String[] args) {
+		
+		sumNumbers(5, 7, 8);
+		sumNumbers(10 ,15, 38);
+		sumNumbers(155 ,2000, 54145);
+		
+		int r1 = randomCreator(500);
+		int r2 = randomCreator(20);
+		int r3 = randomCreator(250);
+		
+		System.out.println(r1 + " r1 ");
+		System.out.println(r2 + " r2 ");
+		System.out.println(r3 + " r3 ");
+		
+		sumNumbers(r1, r2, r3);
+		
+		
+		
+	}
+	
 /*
  * 
  * Create one java method that will sum 3 numbers and print it in main method 
  * 
  */
+	
+	public static void sumNumbers(int i1 , int i2 , int i3) {
+		
+		int total = i1+i2+i3;
+		
+		System.out.println(total);
+		
+	}
 	
 
 /*
@@ -19,6 +49,15 @@ public class MethodDay630Exercise {
  * 		
  * 
  */
+	public static int randomCreator(int max) {
+		
+		Random rnd = new Random();
+		
+		int random1 = rnd.nextInt(max);
+		 
+		return random1;
+		 
+	}
 	
 	
 	
@@ -27,9 +66,12 @@ public class MethodDay630Exercise {
 	
 	
 //	Create 2 string array and contaisn each other 
-
+	
+//	ARRAYS SHOULD BE IN THE MAIN METHOD
+		
 //	ArrayList<String> Array1 = new ArrayList<>();
-//	
+ 
+	
 //	Array1.add("Korea");
 //	Array1.add("Mexico");
 //	Array1.add("USA");
@@ -47,9 +89,6 @@ public class MethodDay630Exercise {
 //	Array2.add("Japan");
 //	Array2.add("Canada");
 //	Array2.add("South Africa");
-	
-	
-//	
 	
 	
 	
