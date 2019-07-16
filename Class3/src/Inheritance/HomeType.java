24b1dffd091e807279e639cdec2f8ab96814838b
mhmmtkkl <makoklu32@gmail.com>
2019-07-15 21:05:45 -0400

    7/15

--- Class3/src/Inheritance/HomeType.java
+++ Class3/src/Inheritance/HomeType.java
@@ -5,7 +5,16 @@ public class HomeType {
 /*
  * HomeType should have 
  * 
- * 	
+ * 	variables floor 
+ * 
+ *  window 
+ *  
+ *  room 
+ *  
+ *  house should have fireplace 
+ *  
+ *  apartment should have neighbor count 
+ *  
  */
 	
 	
